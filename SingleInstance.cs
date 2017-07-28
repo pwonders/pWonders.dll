74af33315dee6c03d825657a749059457331cbea
pwonders <me@plainwonders.net>
2017-07-28 01:36:36 -0700

    Add project files.

---
+++ SingleInstance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace pWonders
+{
+	public class SingleInstance
+	{
+		// See also:
+		// https://stackoverflow.com/questions/229565/what-is-a-good-pattern-for-using-a-global-mutex-in-c/229567
+		public SingleInstance(Action func)
+		{
+			string name = this.GetType().Name + "-";
+			object[] attribs = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false);
+			if (attribs.Length > 0)
+			{
+				name += (attribs[0] as GuidAttribute).Value;
+			}
+			else
+			{
+				name += Assembly.GetExecutingAssembly().GetName().Name;
+			}
+			bool createdNew;
+
+			using (var mutex = new Mutex(false, name, out createdNew))
+			{
+				if (createdNew)
+				{
+					func();
+				}
+			}
+		}
+	}
+}
---
+++ d.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace pWonders
+{
+	public static class d
+	{
+		public static void Win32Error(string func)
+		{
+			Win32Error(func, Marshal.GetLastWin32Error());
+		}
+
+		public static void Win32Error(string func, int err)
+		{
+			Debug.WriteLine("Error " + func + " 0x" + err.ToString("x") + " " + new Win32Exception(err).Message);
+		}
+	}
+}
