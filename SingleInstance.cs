f0cbd10ce419321c696b06801655821780fb047d
pwonders <me@plainwonders.net>
2017-08-19 20:58:14 -0700

    exposed unique name for extern use (settings tile name).

--- SingleInstance.cs
+++ SingleInstance.cs
@@ -1,29 +1,44 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 
 namespace pWonders
 {
 	public class SingleInstance
 	{
+		public static string GetUniqueName()
+		{
+			AssemblyName an = Assembly.GetExecutingAssembly().GetName();
+			string name = an.Name + "-";
+			object[] attribs = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false);
+			if (attribs.Length > 0)
+			{
+				name += (attribs[0] as GuidAttribute).Value.Replace("-", "");
+			}
+			else
+			{
+				StringBuilder sb = new StringBuilder(32);
+				Array.ForEach
+				(
+					MD5.Create().ComputeHash(Encoding.UTF32.GetBytes(an.FullName)), b =>
+					{
+						sb.Append(b.ToString("x2"));
+					}
+				);
+				name += sb.ToString();
+			}
+			return name;
+		}
+
 		// See also:
 		// https://stackoverflow.com/questions/229565/what-is-a-good-pattern-for-using-a-global-mutex-in-c/229567
 		public SingleInstance(Action func)
 		{
-			string name = this.GetType().Name + "-";
-			object[] attribs = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false);
-			if (attribs.Length > 0)
-			{
-				name += (attribs[0] as GuidAttribute).Value;
-			}
-			else
-			{
-				name += Assembly.GetExecutingAssembly().GetName().Name;
-			}
 			bool createdNew;
-
-			using (var mutex = new Mutex(false, name, out createdNew))
+			using (var mutex = new Mutex(false, GetUniqueName(), out createdNew))
 			{
 				if (createdNew)
 				{
